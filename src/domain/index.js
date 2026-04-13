c6faad9bbb93562a2225420ad0c4daab7b6b5b10
adksjksjfls65 <180636338@qq.com>
2026-04-13 10:42:35 +0800

    Update index.js

--- src/domain/index.js
+++ src/domain/index.js
@@ -1,5 +1,6 @@
-import { Sudoku } from '../models/sudoku'; // 注意这里变小写了
-import { Game } from '../models/game';     // 注意这里变小写了
+// 注意这里的路径：从 domain 文件夹向上跳一级到 src，再进入那个特殊的 node_modules 目录
+import { Sudoku } from '../node_modules/@sudoku/sudoku';
+import { Game } from '../node_modules/@sudoku/game';
 
 export function createSudoku(grid) {
     return new Sudoku(grid);
